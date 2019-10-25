c5ca6fb87c3efbd3f45f31498a6b44fa0d2cfb87
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-10-24 19:17:24 -0500

    consumed game components

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+import CharacterView from './components/CharacterView';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <CharacterView/>
+      <Footer />
+    </>
+
+  );
+}
+
+export default App;
