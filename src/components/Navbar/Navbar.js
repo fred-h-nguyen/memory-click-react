11a1360d4dc0ab0b19c4ad3638f5b46fe9db354c
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-10-24 19:19:08 -0500

    navbar showing score and reseting game

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Menu, Container, Grid } from 'semantic-ui-react'
+
+class Navbar extends React.Component {
+    state = {}
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    render() {
+        const { activeItem } = this.state
+
+        return (
+            <Menu inverted>
+                <Menu.Item
+                    name='Clicky Game'
+                    active={activeItem === 'clicky'}
+                    onClick={this.handleItemClick}
+                />
+
+                <Grid columns={2}>
+                    <Grid.Column>
+                        <Container textAlign='right'>
+                            <h5
+                                style={{ color: 'white', marginTop: '10px', marginRight:'10px' }}
+                            >
+                                click picture to start!
+                            </h5>
+                        </Container>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Container textAlign='right'>
+                            <h5
+                                style={{ color: 'white', marginTop: '10px', marginRight:'10px' }}
+                            >
+                                Score:0 | Top Score: 0
+                            </h5>
+                        </Container>
+                    </Grid.Column>
+                </Grid>
+            </Menu>
+        )
+    }
+}
+export default Navbar
